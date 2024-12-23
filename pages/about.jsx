161c5cc14e69533f9080c447d753b3af4ef34471
athervisingh <singhathervi001@gmail.com>
2024-12-23 05:33:45 +0530

    finished

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import SyntaxHighlighter from "react-syntax-highlighter/dist/cjs/prism";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
 
 const AboutPage = () => {
   const portfolio = {
